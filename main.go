0e0b8e32c219b9be6c82d8700ab6608e0e155fbf
Алмаз Задин <almaz@MacBook-Air-Almaz.local>
2023-07-20 23:17:58 +0300

    2 err

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ func main() {
 	var a, b, result int
 	var s string
 	fmt.Scan(&a)
+
 	fmt.Scan(&s)
 	fmt.Scan(&b)
+	if a < 0 || b < 0 || a > 10 || b > 10 {
+		panic("Ошибка, цифры не валидны")
+	}
 	switch s {
 	case "+":
 		result = a + b
@@ -17,6 +21,8 @@ func main() {
 		result = a * b
 	case "/":
 		result = a / b
+	default:
+		panic("Операнд не валиден")
 	}
 	fmt.Printf("%d %s %d = %d\n", a, s, b, result)
 }
